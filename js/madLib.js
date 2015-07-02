2c41cafcf5d2418ff21ed8d11ce6f35b0ec96853
Tyler Silva <tylerbsilva@gmail.com>
2015-07-01 20:36:40 -0500

    Removing unnecessary code

--- js/madLib.js
+++ js/madLib.js
@@ -1,16 +1,5 @@
 var app = angular.module('madLib', []);
 app.controller('controller', ['$scope', function($scope){
-  // $scope.heShe = 'she';
-  // $scope.himHer = 'her';
-  // $scope.name = 'Name';
-  // $scope.jobTitle = 'Job Title';
-  // $scope.tediousTask = 'Tedious Task';
-  // $scope.dirtyTask = 'Dirty Task';
-  // $scope.celebrity = 'Celebrity';
-  // $scope.uselessSkill = 'Useless Skill';
-  // $scope.adjective = 'Adjective';
-  // $scope.obnoxiuousCelebrity = 'Obnoxiuous Celebrity';
-  // $scope.hugeNumber = 'Huge Number';
   $scope.inputVisable = 'visable';
   $scope.outputVisable  = '';
   $scope.gender = '';
@@ -23,7 +12,6 @@ app.controller('controller', ['$scope', function($scope){
     }
   }
   $scope.male = function(){
-    $scope.check = "male"
     $scope.heShe = 'he';
     $scope.himHer = 'him';
   };
