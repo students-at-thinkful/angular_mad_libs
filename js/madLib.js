2ebcbff2b14bdf766f776e057172c835d5672c7a
Tyler Silva <tylerbsilva@gmail.com>
2015-07-01 20:02:51 -0500

    Added better UI and a little bit of animation

--- js/madLib.js
+++ js/madLib.js
@@ -1,12 +1,36 @@
 var app = angular.module('madLib', []);
 app.controller('controller', ['$scope', function($scope){
-  $scope.femaleName = 'Female Name';
-  $scope.jobTitle = 'Job Title';
-  $scope.tediousTask = 'Tedious Task';
-  $scope.dirtyTask = 'Dirty Task';
-  $scope.celebrity = 'Celebrity';
-  $scope.uselessSkill = 'Useless Skill';
-  $scope.adjective = 'Adjective';
-  $scope.obnoxiuousCelebrity = 'Obnoxiuous Celebrity';
-  $scope.hugeNumber = 'Huge Number';
+  // $scope.heShe = 'she';
+  // $scope.himHer = 'her';
+  // $scope.name = 'Name';
+  // $scope.jobTitle = 'Job Title';
+  // $scope.tediousTask = 'Tedious Task';
+  // $scope.dirtyTask = 'Dirty Task';
+  // $scope.celebrity = 'Celebrity';
+  // $scope.uselessSkill = 'Useless Skill';
+  // $scope.adjective = 'Adjective';
+  // $scope.obnoxiuousCelebrity = 'Obnoxiuous Celebrity';
+  // $scope.hugeNumber = 'Huge Number';
+  $scope.inputVisable = 'visable';
+  $scope.outputVisable  = '';
+  $scope.gender = '';
+
+  $scope.male = function(){
+    $scope.check = "male"
+    $scope.heShe = 'he';
+    $scope.himHer = 'him';
+  };
+  $scope.female = function(){
+    $scope.heShe = 'she';
+    $scope.himHer = 'her';
+  };
+  $scope.switch = function(){
+    if ($scope.inputVisable){
+      $scope.inputVisable = '';
+      $scope.outputVisable = 'visable';
+    } else {
+      $scope.inputVisable = 'visable';
+      $scope.outputVisable = '';
+    }
+  };
 }]);
